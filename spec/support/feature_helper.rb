e04bbe5c1a4d9dc1022de34438065961ff4fe08c
jpalmieri <jpalmieri@lumoslabs.com>
2015-01-23 13:46:39 -0800

    Sign in flow

---
+++ spec/support/feature_helper.rb
@@ -0,0 +1,18 @@
+module SignInHelper
+
+  def sign_in(user)
+    visit root_path
+
+    within '.user-info' do
+      click_link 'Sign In'
+    end
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+
+    within 'form' do
+      click_button 'Log in'
+    end
+    expect( page ).to have_content("Signed in successfully.")
+  end
+  
+end
