6f87a9f1806872dffa3cd923d635dc05aeacf37b
jpalmieri <jpalmieri@lumoslabs.com>
2015-01-23 16:25:07 -0800

    new recipes created under users

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -5,7 +5,7 @@ class RecipesController < ApplicationController
   end
 
   def create
-    @recipe = Recipe.new(recipe_params)
+    @recipe = current_user.recipes.create(recipe_params)
     if @recipe.save
       flash[:notice] = "Recipe saved."
       redirect_to @recipe
--- spec/features/user_creates_recipes_spec.rb
+++ spec/features/user_creates_recipes_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
 feature "User creates recipes", :type => :feature do
+
+  before do
+    user = create(:user)
+    sign_in(user)
+  end
   
   scenario "successfully" do
     visit new_recipe_path
