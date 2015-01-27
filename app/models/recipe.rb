610a822edef23ac0f597c0ba7c3eb22d79d69d7a
jpalmieri <jpalmieri@lumoslabs.com>
2015-01-27 15:38:16 -0800

    custom error message for grain validation

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,10 +1,10 @@
 class Recipe < ActiveRecord::Base
   belongs_to :user
   has_many :grains, dependent: :destroy
-  accepts_nested_attributes_for :grains
+  accepts_nested_attributes_for :grains, reject_if: proc { |attributes| attributes['name'].blank? || attributes['weight'].blank? }
 
   validates :name, presence: true
   validates :user_id, presence: true
-  validates :grains, presence: true
+  validates_presence_of :grains,  message: "must include name and weight of at least one grain"
   
 end
--- spec/factories/grain.rb
+++ spec/factories/grain.rb
@@ -3,5 +3,6 @@ require 'factory_girl'
 FactoryGirl.define do
   factory :grain do
     name "2-row"
+    weight 1
   end
 end
