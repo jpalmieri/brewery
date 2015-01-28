6576d85a7bf7bc70fb3e2aa61c241e3f625575d8
jpalmieri <jpalmieri@lumoslabs.com>
2015-01-28 12:32:48 -0800

    extended recipe#new feature specs and made more specific specs

--- spec/features/user_creates_recipes_spec.rb
+++ spec/features/user_creates_recipes_spec.rb
@@ -1,24 +1,24 @@
 require 'rails_helper'
 
-feature "User creates recipes", :type => :feature do
+feature "User creates recipe", :type => :feature do
 
   before do
     user = create(:user)
     sign_in(user)
+    visit new_recipe_path
+    fill_in "Style", with: "Stout"
+    fill_in "Yeast", with: "Nottingham"
+    fill_in "Summary", with: "A very dark stout"
+    fill_in "Notes", with: "Brew very carefully..."
   end
   
   scenario "successfully" do
-    visit new_recipe_path
     fill_in "Name", with: "Black Stout"
-    fill_in "Style", with: "Stout"
-    fill_in "Yeast", with: "Nottingham"
     fill_in "Grain 1 name", with: "2-row"
     fill_in "Grain 1 weight", with: "10"
     fill_in "Grain 2 name", with: "Crystal 40L"
     fill_in "Grain 2 weight", with: "2.25" 
-    fill_in "Summary", with: "A very dark stout"
-    fill_in "Notes", with: "Brew very carefully..."
-    
+
     click_button "Save Recipe"
 
     expect(Recipe.count).to eq(1)
@@ -30,22 +30,86 @@ feature "User creates recipes", :type => :feature do
     expect(page).to have_content("Crystal 40L")
     expect(page).to have_content("2.25")
     expect(page).to have_content("A very dark stout")
-    expect(page).to have_content("Black Stout")
     expect(page).to have_content("Brew very carefully...")
     expect(page).to have_content("Recipe saved.")
   end
 
-  scenario "unsuccessfully" do 
-    visit new_recipe_path
-    fill_in "Yeast", with: "Nottingham"
+  scenario "unsuccessfully, no recipe name" do 
+    fill_in "Grain 1 name", with: "2-row"
+    fill_in "Grain 1 weight", with: "10"
+    fill_in "Grain 2 name", with: "Crystal 40L"
+    fill_in "Grain 2 weight", with: "2.25" 
 
     click_button "Save Recipe"
 
     expect(current_path).to eq recipes_path
     expect(page).to have_content("Name can't be blank")
-    expect(page).to have_content("must include name and weight of at least one grain")
+    expect(page).to have_selector("input[value='Stout']")
     expect(page).to have_selector("input[value='Nottingham']")
-    # grain inputs must reappear on render: new
+    expect(page).to have_selector("input[value='2-row']")
+    expect(page).to have_selector("input[value='10']")
+    expect(page).to have_selector("input[value='Crystal 40L']")
+    expect(page).to have_selector("input[value='2.25']")
+    expect(page).to have_selector("input[value='A very dark stout']")
+    expect(page).to have_selector("input[value='Brew very carefully...']")
+    # unused grain inputs must reappear on render: new
+    expect(page).to have_content("Grain 3 name")
+    expect(page).to have_content("Grain 3 weight")
+  end
+
+  scenario "unsuccessfully, no grains at all" do 
+    fill_in "Name", with: "Black Stout"
+    click_button "Save Recipe"
+
+    expect(current_path).to eq recipes_path
+    expect(page).to have_content("must include name and weight of at least one grain")
+    expect(page).to have_selector("input[value='Stout']")
+    expect(page).to have_selector("input[value='Nottingham']")
+    expect(page).to have_selector("input[value='A very dark stout']")
+    expect(page).to have_selector("input[value='Brew very carefully...']")
+    # unused grain inputs must reappear on render: new
+    expect(page).to have_content("Grain 1 name")
+    expect(page).to have_content("Grain 1 weight")
+    expect(page).to have_content("Grain 2 name")
+    expect(page).to have_content("Grain 2 weight")
+    expect(page).to have_content("Grain 3 name")
+    expect(page).to have_content("Grain 3 weight")
+  end
+
+  scenario "unsuccessfully, grain name but no weight" do 
+    fill_in "Name", with: "Black Stout"
+    fill_in "Grain 1 name", with: "2-row"
+    click_button "Save Recipe"
+
+    expect(current_path).to eq recipes_path
+    expect(page).to have_content("Grains weight can't be blank")
+    expect(page).to have_selector("input[value='Stout']")
+    expect(page).to have_selector("input[value='Nottingham']")
+    expect(page).to have_selector("input[value='2-row']")
+    expect(page).to have_selector("input[value='A very dark stout']")
+    expect(page).to have_selector("input[value='Brew very carefully...']")
+    # unused grain inputs must reappear on render: new
+    expect(page).to have_content("Grain 1 name")
+    expect(page).to have_content("Grain 1 weight")
+    expect(page).to have_content("Grain 2 name")
+    expect(page).to have_content("Grain 2 weight")
+    expect(page).to have_content("Grain 3 name")
+    expect(page).to have_content("Grain 3 weight")
+  end
+
+  scenario "unsuccessfully, grain weight but no name" do 
+    fill_in "Name", with: "Black Stout"
+    fill_in "Grain 1 weight", with: "10"
+    click_button "Save Recipe"
+
+    expect(current_path).to eq recipes_path
+    expect(page).to have_content("Grains name can't be blank")
+    expect(page).to have_selector("input[value='Stout']")
+    expect(page).to have_selector("input[value='Nottingham']")
+    expect(page).to have_selector("input[value='10']")
+    expect(page).to have_selector("input[value='A very dark stout']")
+    expect(page).to have_selector("input[value='Brew very carefully...']")
+    # unused grain inputs must reappear on render: new
     expect(page).to have_content("Grain 1 name")
     expect(page).to have_content("Grain 1 weight")
     expect(page).to have_content("Grain 2 name")
