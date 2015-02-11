c3866c7bf0413cd735937139b70544a8bb65a917
jpalmieri <jpalmieri@lumoslabs.com>
2015-02-11 10:24:39 -0800

    installed poletergeist

--- Gemfile
+++ Gemfile
@@ -17,6 +17,7 @@ group :test do
   gem 'factory_girl_rails'
   gem 'database_cleaner'
   gem 'shoulda-matchers', require: false
+  gem 'poltergeist'
 end
 
 group :production do
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,9 @@ require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'capybara/rails'
 require 'shoulda/matchers'
+require 'capybara/poltergeist'
+Capybara.javascript_driver = :poltergeist
+
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
